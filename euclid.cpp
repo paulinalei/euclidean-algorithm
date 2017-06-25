fd6193f1971e3a6ec771bfc4a252985c74908000
Paulina Lei <paulina.y.lei@gmail.com>
2017-06-25 21:31:24 +0100

    implementation done

--- euclid.cpp
+++ euclid.cpp
@@ -5,14 +5,28 @@ Euclid created an algorithm that could find the greatest common divisor of any t
 */
 
 #include <iostream>
+using namespace std;
 
-int euclid(int a, int b) {
-  int gcd = 0;
-  cout << "Please enter in two numbers" << endl;
-  cin >> a >> b;
-  return gcd;
+int euclid(int input1, int input2) {
+  int gcd = 1;
+
+  while (input1 != input2) {
+    if (input1 > input2) {
+      input1 -= input2;
+    } else {
+      input2 -= input1;
+    }
+  }
+  return input1;
 }
 
 int main() {
+  cout << "Euclid function call" << endl;
+  cout << "Please enter in two numbers" << endl;
+  int a = 0;
+  int b = 0;
+  cin >> a >> b;
+  int result = euclid(a, b);
+  cout << result << endl;
   return 0;
 }
